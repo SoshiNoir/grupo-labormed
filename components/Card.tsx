3140eba46e615011906ed5ceb21d77b43ac26aea
Miguel Matias <migbonamim@gmail.com>
2025-04-08 08:18:32 -0300

    hover added to cards

--- components/Card.tsx
+++ components/Card.tsx
@@ -6,15 +6,19 @@ interface CardProps {
   rightContent: React.ReactNode;
 }
 
-const Card: React.FC<CardProps> = ({ className = '', leftContent, rightContent }) => {
+const Card: React.FC<CardProps> = ({
+  className = '',
+  leftContent,
+  rightContent,
+}) => {
   return (
-    <div className={`flex flex-col rounded-md overflow-hidden my-12 ${className} md:flex-row`}>
-      <div className="flex w-full p-4 md:w-3/5">
-        {leftContent}
-      </div>
-      <div className="flex w-full p-4 md:w-2/5">
-        {rightContent}
-      </div>
+    <div
+      className={`relative group flex flex-col rounded-md overflow-hidden my-12 transition duration-300 ${className} md:flex-row`}
+    >
+      <div className='absolute inset-0 bg-black/20 opacity-0 group-hover:opacity-100 transition duration-300 pointer-events-none z-10' />
+
+      <div className='flex w-full p-4 md:w-3/5 z-20'>{leftContent}</div>
+      <div className='flex w-full p-4 md:w-2/5 z-20'>{rightContent}</div>
     </div>
   );
 };
--- components/Services.tsx
+++ components/Services.tsx
@@ -39,23 +39,26 @@ const images = [
 
 const ImageGallery = () => {
   return (
-    <div className="flex flex-col items-center mt-8 px-4">
-      <div className="w-full flex justify-center">
-        <div className="grid grid-cols-1 gap-4 w-full sm:grid-cols-2 lg:grid-cols-4">
+    <div className='flex flex-col items-center mt-8 px-4'>
+      <div className='w-full flex justify-center'>
+        <div className='grid grid-cols-1 gap-4 w-full sm:grid-cols-2 lg:grid-cols-4'>
           {images.map((image) => (
             <Link key={image.id} href={image.link} passHref>
-              <div className="relative w-full h-[400px] max-w-[400px] mx-auto overflow-hidden rounded-lg cursor-pointer">
-                <div className="relative w-full h-full">
+              <div className='group relative w-full h-[400px] max-w-[400px] mx-auto overflow-hidden rounded-lg cursor-pointer transition duration-300'>
+                <div className='absolute inset-0 bg-black/20 opacity-0 group-hover:opacity-100 transition duration-300 z-10 pointer-events-none' />
+
+                <div className='relative w-full h-full z-20'>
                   <Image
                     src={image.src}
                     alt={image.alt}
-                    objectFit="cover"
-                    className="absolute inset-0"
-                    fill />
+                    objectFit='cover'
+                    className='absolute inset-0'
+                    fill
+                  />
                   <div
                     className={`absolute bottom-0 left-0 right-0 h-1/3 ${image.gradient} flex items-end justify-center p-4`}
                   >
-                    <h3 className="text-[30px] text-center text-white font-bold font-RobotoMono">
+                    <h3 className='text-[30px] text-center text-white font-bold font-RobotoMono'>
                       {image.title}
                     </h3>
                   </div>
